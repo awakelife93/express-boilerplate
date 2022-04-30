f55df20605e88cb4d3c965fcb41c13eb2d206b1d
hw.park <hw.park@fastfive.co.kr>
2022-04-30 20:52:23 +0900

    type 추가로 인한 쓸모없는 주석 제거

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,26 +5,19 @@ import "dotenv/config";
  * 외부에 노출되는 데이터들이기 때문에 주의해서 사용해야 한다.
  */
 const config: ConfigType = {
-  // "development" | "production" | "localhost"
   NODE_ENV: (process.env.NODE_ENV as NodeEnvType) ?? "localhost",
-  // sentry dsn
   sentryDSN: process.env.dsn ?? "",
-  // react port를 3000으로 준다.
   port: process.env.port ?? 3000,
-  // redis
   redisPort: process.env.redisPort ?? 6379,
   redisHost: process.env.redisHost ?? "127.0.0.1",
-  // mysql
   mysqlUserName: process.env.mysqlUserName ?? "root",
   mysqlPort: process.env.mysqlPort ?? 3306,
   mysqlHost: process.env.mysqlHost ?? "127.0.0.1",
   mysqlDataBase: process.env.mysqlDataBase ?? "localDB",
   mysqlPassword: process.env.mysqlPassword ?? "",
-  // jwt
   jwtSecret: process.env.jwtSecret ?? "secret",
   jwtExpired: process.env.jwtExpired ?? "1h",
   jwtRefreshExpired: process.env.jwtRefreshExpired ?? "3h",
-  // domain
   origin:
     process.env.gatewayDomain && process.env.gatewayPort
       ? `http://${process.env.gatewayDomain}:${process.env.gatewayPort}`
