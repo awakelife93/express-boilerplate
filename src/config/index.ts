f421d0d97242d926ffc265a883990662065a120d
harry <awakelife93@gmail.com>
2022-05-17 20:23:23 +0900

    config type 위치 이동 및 redis expire time을 ms로 수정

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,27 @@
-import { ConfigType, NodeEnvType } from "@/lib/type";
+import { NodeEnvType } from "@/lib/type";
 import "dotenv/config";
 
 /**
  * 외부에 노출되는 데이터들이기 때문에 주의해서 사용해야 한다.
  */
+export type ConfigType = {
+  NODE_ENV: NodeEnvType;
+  sentryDSN: string;
+  port: string | number;
+  redisPort: string | number;
+  redisHost: string;
+  mysqlPort: string | number;
+  mysqlHost: string;
+  mysqlUserName: string;
+  mysqlDataBase: string;
+  mysqlPassword: string;
+  jwtSecret: string;
+  jwtExpireMS: number;
+  jwtRefreshExpireMS: number;
+  origin: string;
+  timezone: string;
+};
+
 const config: ConfigType = {
   NODE_ENV: (process.env.NODE_ENV as NodeEnvType) ?? "localhost",
   sentryDSN: process.env.dsn ?? "",
@@ -16,8 +34,12 @@ const config: ConfigType = {
   mysqlDataBase: process.env.mysqlDataBase ?? "localDB",
   mysqlPassword: process.env.mysqlPassword ?? "",
   jwtSecret: process.env.jwtSecret ?? "secret",
-  jwtExpired: process.env.jwtExpired ?? "1h",
-  jwtRefreshExpired: process.env.jwtRefreshExpired ?? "3h",
+  jwtExpireMS: process.env.jwtExpireMS
+    ? Number(process.env.jwtExpireMS)
+    : 60000 * 60, // 1h
+  jwtRefreshExpireMS: process.env.jwtRefreshExpireMS
+    ? Number(process.env.jwtRefreshExpireMS)
+    : 60000 * 60 * 3, // 3h
   origin:
     process.env.gatewayDomain && process.env.gatewayPort
       ? `http://${process.env.gatewayDomain}:${process.env.gatewayPort}`
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -5,21 +5,3 @@ export type UnknownObject<T = unknown> = Record<string, T>;
 export type CommonAPIResponseType<T> = T | undefined;
 
 export type CommonPromiseAPIResponseType<T> = Promise<T | undefined>;
-
-export type ConfigType = {
-  NODE_ENV: NodeEnvType;
-  sentryDSN: string;
-  port: string | number;
-  redisPort: string | number;
-  redisHost: string;
-  mysqlPort: string | number;
-  mysqlHost: string;
-  mysqlUserName: string;
-  mysqlDataBase: string;
-  mysqlPassword: string;
-  jwtSecret: string;
-  jwtExpired: string | number;
-  jwtRefreshExpired: string | number;
-  origin: string;
-  timezone: string;
-};
