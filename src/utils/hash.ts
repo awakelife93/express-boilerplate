fea33af9874e351bc1423003f22eda1bba367bf4
hw.park <hw.park@fastfive.co.kr>
2022-07-12 22:01:28 +0900

    bcrypt -> bcryptjs로 수정

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export const hashSync = (data: string, saltRounds: number = 10): string => {
   return bcrypt.hashSync(data, saltRounds);
