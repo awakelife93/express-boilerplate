8428e36932f025880b3600cb2333c8211cc94a05
hw.park <hw.park@fastfive.co.kr>
2022-08-11 13:19:51 +0900

    compose file 수정 및 config 수정 (docker 환경변수는 undefined가 아닌 blank...)

--- src/config/index.ts
+++ src/config/index.ts
@@ -19,23 +19,24 @@ export type Config = {
   origin: string;
 };
 
+// * Because variables are injected even in the Docker environment, || is used because it is an empty string rather than undefined.
 const config: Config = {
-  NODE_ENV: (process.env.NODE_ENV as NodeEnvironment) ?? "localhost",
+  NODE_ENV: (process.env.NODE_ENV as NodeEnvironment) || "localhost",
   sentryDSN: process.env.sentryDSN,
-  port: process.env.port ?? 3000,
+  port: process.env.port || 3000,
   redis: {
-    host: process.env.redisHost ?? "127.0.0.1",
+    host: process.env.redisHost || "127.0.0.1",
     port: process.env.redisPort ? Number(process.env.redisPort) : 6379,
   },
   mysql: {
     type: "mysql",
     port: process.env.mysqlPort ? Number(process.env.mysqlPort) : 3306,
-    host: process.env.mysqlHost ?? "127.0.0.1",
+    host: process.env.mysqlHost || "127.0.0.1",
     username: process.env.mysqlUserName,
     database: process.env.mysqlDataBase,
     password: process.env.mysqlPassword,
   },
-  jwtSecret: process.env.jwtSecret ?? "secret",
+  jwtSecret: process.env.jwtSecret || "secret",
   jwtExpireMS: process.env.jwtExpireMS
     ? Number(process.env.jwtExpireMS)
     : 60000 * 60, // 1h
